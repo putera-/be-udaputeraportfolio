dea34e078d2da4576039fd949cc4f40542138607
Rizjami Putera <rizjamiputera@gmail.com>
2023-12-22 19:03:36 +0700

    fix migration conflict

--- prisma/migrations/20231221183831_create_skill_table/migration.sql
+++ prisma/migrations/20231221183831_create_skill_table/migration.sql
@@ -11,10 +11,10 @@ CREATE TABLE `skill_category` (
 CREATE TABLE `skill` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `title` VARCHAR(100) NOT NULL,
-    `category_id` INTEGER NOT NULL,
+    `categoryId` INTEGER NOT NULL,
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- AddForeignKey
-ALTER TABLE `skill` ADD CONSTRAINT `skill_category_id_fkey` FOREIGN KEY (`category_id`) REFERENCES `skill_category`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `skill` ADD CONSTRAINT `skill_categoryId_fkey` FOREIGN KEY (`categoryId`) REFERENCES `skill_category`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
