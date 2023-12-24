b2eedde9f6072d23a221e8a93789b8b9c509bbf3
Rizjami Putera <rizjamiputera@gmail.com>
2023-12-24 21:28:23 +0700

    fix password validation

--- src/validation/all-validation.js
+++ src/validation/all-validation.js
@@ -3,7 +3,7 @@ import Joi from "joi"
 const istruthy = Joi.boolean().truthy();
 const isID = Joi.number().positive().required();
 const isEmail = Joi.string().email().max(100).trim().required();
-const isPassword = Joi.string().min(6).max(100).trim().required();
+const isPassword = Joi.string().min(6).max(100).trim();
 const isString = Joi.string().min(3).max(100).trim().required();
 const isText = Joi.string().trim();
 const isURL = Joi.string().uri();
