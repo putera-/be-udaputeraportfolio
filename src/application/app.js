4c55dbe7b1fbd9273aafaa13543ba414c91f52bb
Rizjami Putera <rizjamiputera@gmail.com>
2023-12-27 23:04:07 +0700

    create not found route

--- src/application/app.js
+++ src/application/app.js
@@ -3,6 +3,7 @@ import cookieParser from "cookie-parser";
 import { publicRouter } from "../route/public-api.js";
 import { errorMiddleware } from "../middleware/error-middleware.js";
 import { apiRouter } from "../route/api.js";
+import { notFound } from "../middleware/notfound-middleware.js";
 
 export const app = express();
 
@@ -11,6 +12,7 @@ app.use(cookieParser())
 
 app.use(publicRouter);
 app.use(apiRouter);
+app.use(notFound);
 
 // handle error
 app.use(errorMiddleware);
