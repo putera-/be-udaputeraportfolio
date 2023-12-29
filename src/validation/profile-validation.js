d8a689f4dd2c7d97d72a86576112625a95dc2a5e
Rizjami Putera <rizjamiputera@gmail.com>
2023-12-29 07:53:11 +0700

    Updare profile validation

--- src/validation/profile-validation.js
+++ src/validation/profile-validation.js
@@ -1,11 +1,17 @@
 import Joi from "joi";
-import { isEmail, isString } from "./all-validation.js";
+import { isEmail, isString, isURL } from "./all-validation.js";
 
 const profileValidate = Joi.object({
     firstname: isString,
     lastname: isString,
     email: isEmail,
-
+    bio: Joi.string().trim(),
+    github: isURL,
+    gitlab: isURL,
+    linkedin: isURL,
+    instagram: isURL,
+    facebook: isURL,
+    twitter: isURL
 });
 
 export {
