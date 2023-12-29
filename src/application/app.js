804d304d28195632aee67aa7593c1b996d6d6095
Rizjami Putera <rizjamiputera@gmail.com>
2023-12-29 07:52:50 +0700

    Add cors

--- src/application/app.js
+++ src/application/app.js
@@ -4,11 +4,26 @@ import { publicRouter } from "../route/public-api.js";
 import { errorMiddleware } from "../middleware/error-middleware.js";
 import { apiRouter } from "../route/api.js";
 import { notFound } from "../middleware/notfound-middleware.js";
+import cors from 'cors';
 
 export const app = express();
 
 app.use(express.json());
 app.use(cookieParser())
+
+// cors
+if (process.env.NODE_ENV == 'development') {
+    console.log('set cors development')
+    app.use(cors({
+        origin: ['http://localhost:3000', 'http://localhost:3001', 'http://localhost:3003'],
+        credentials: true
+    }));
+} else if (process.env.NODE_ENV == 'production') {
+    app.use(cors({
+        origin: [process.env.APP_WEB_URL],
+        credentials: true
+    }));
+}
 
 app.use(publicRouter);
 app.use(apiRouter);
