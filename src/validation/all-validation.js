4983b0cfd97fe258f56f1f911cb4492ae3365731
Rizjami Putera <rizjamiputera@gmail.com>
2024-01-06 13:26:31 +0700

    updat joi for url

--- src/validation/all-validation.js
+++ src/validation/all-validation.js
@@ -5,8 +5,8 @@ const isID = Joi.number().positive().required();
 const isEmail = Joi.string().email().max(100).trim().required();
 const isPassword = Joi.string().min(6).max(100).trim();
 const isString = Joi.string().min(3).max(100).trim().required();
-const isText = Joi.string().min(3).trim();
-const isURL = Joi.string().uri();
+const isText = Joi.string().trim().min(3);
+const isURL = Joi.string().trim().uri();
 
 export {
     istruthy,
