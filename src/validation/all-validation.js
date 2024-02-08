8957396a44a2bcf1ee221955099ba4c4164b95c4
Rizjami Putera <rizjamiputera@gmail.com>
2024-02-09 01:55:00 +0700

    update

--- src/validation/all-validation.js
+++ src/validation/all-validation.js
@@ -1,11 +1,11 @@
 import Joi from 'joi';
 
 const istruthy = Joi.boolean().truthy();
-const isID = Joi.number().positive().required();
-const isEmail = Joi.string().email().max(100).trim().required();
+const isID = Joi.number().positive();
+const isEmail = Joi.string().email().max(100).trim();
 const isPassword = Joi.string().min(6).max(100).trim();
-const isString = Joi.string().min(3).max(100).trim().required();
-const isText = Joi.string().trim().min(3);
+const isString = Joi.string().min(3).max(100).trim();
+const isText = Joi.string().trim();
 const isURL = Joi.string().trim().uri();
 
 export {
