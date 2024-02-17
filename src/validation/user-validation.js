23a33fe48665227bfae342e58766d799af4e9c0c
Rizjami Putera <rizjamiputera@gmail.com>
2024-02-17 13:10:15 +0700

    update user password validation

--- src/validation/user-validation.js
+++ src/validation/user-validation.js
@@ -2,17 +2,16 @@ import Joi from 'joi';
 import { isPassword, isString } from './all-validation.js';
 
 const updateUserValidation = Joi.object({
-    name: isString.label("Name"),
-    password: isPassword.label("Password"),
-    password_confirm: isPassword.label("Password Confirm")
-}).custom((value, helpers) => {
-    if (value.password !== value.password_confirm) {
-        return helpers.error('register.password.different');
-    }
-
-    return value;
-}).message({
-    'register.password.different': 'Password and Password Confirm is not match'
+    name: isString.required().label("Name"),
+    password: isPassword.required().label("Password"),
+    password_confirm: isPassword.required()
+        .valid(Joi.ref('password'))
+        .label("Password Confirm")
+        .options({
+            messages: {
+                'any.only': '{{#label}} is not match'
+            }
+        })
 });
 
 export {
