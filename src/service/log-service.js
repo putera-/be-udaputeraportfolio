51c0e3032281c1e7a3c224d192e840b5e947052e
Rizjami Putera <rizjamiputera@gmail.com>
2024-02-22 09:05:16 +0700

    grouping web session logs

--- src/service/log-service.js
+++ src/service/log-service.js
@@ -25,18 +25,51 @@ const getErrorLog = () => {
 };
 
 const getWebAccessLog = async () => {
-    const logs = await prismaClient.accessLog.findMany({
-        orderBy: {
-            timestamp: 'desc'
-        }
+    const log_sessions = await prismaClient.accessLog.groupBy({
+        by: ['session'],
+        _max: {
+            timestamp: true
+        },
+        _count: {
+            _all: true
+        },
     });
 
-    for (const log of logs) {
-        log.readDate = dayjs(log.timestamp).format('D MMMM YYYY')
-        log.readTime = dayjs(log.timestamp).format('HH:ss')
+    for (const session of log_sessions) {
+        const logs = await prismaClient.accessLog.findMany({
+            where: {
+                session: session.session
+            },
+            orderBy: {
+                timestamp: 'desc'
+            }
+        });
+
+        for (const log of logs) {
+            log.readDate = dayjs(log.timestamp).format('D MMMM YYYY');
+            log.readTime = dayjs(log.timestamp).format('HH:mm');
+        }
+
+        session.logs = logs;
+
+
+        // ip & location
+        session.ip = logs.length ? logs[0].ip : '';
+        session.city = logs.length ? logs[0].city : '';
+        session.country = logs.length ? logs[0].country : '';
+        session.countryCode = logs.length ? logs[0].countryCode : '';
+
+        // timestamp
+        session.timestamp = session._max.timestamp;
+        session.readDate = dayjs(session.timestamp).format('D MMMM YYYY');
+        session.readTime = dayjs(session.timestamp).format('HH:mm');
+        session.count = session._count._all;
+
+        delete session._count;
+        delete session._max;
     }
 
-    return logs;
+    return log_sessions;
 };
 
 export default {
