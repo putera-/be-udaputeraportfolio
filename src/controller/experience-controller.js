f3b4f2e6b418aeeced47b27c8751965eed597180
Rizjami Putera <rizjamiputera@gmail.com>
2024-02-24 10:16:44 +0700

    fix return data

--- src/controller/experience-controller.js
+++ src/controller/experience-controller.js
@@ -33,7 +33,7 @@ const create = async (req, res, next) => {
     try {
         const data = await experienceService.create(req.body);
 
-        res.status(200).json({ data });
+        res.status(200).json(data);
     } catch (error) {
         next(error);
     }
@@ -44,7 +44,7 @@ const update = async (req, res, next) => {
         const id = req.params.id;
         const data = await experienceService.update(id, req.body);
 
-        res.status(200).json({ data });
+        res.status(200).json(data);
     } catch (error) {
         next(error);
     }
