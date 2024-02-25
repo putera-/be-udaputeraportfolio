3172150e982b2430f7fb643fc2aed618ec061d9d
Rizjami Putera <rizjamiputera@gmail.com>
2024-02-26 00:35:59 +0700

    dissable mysql winston

--- src/application/logging.js
+++ src/application/logging.js
@@ -1,5 +1,5 @@
 import winston from 'winston';
-import MySQLTransport from 'winston-mysql/lib/mysql_transport.js';
+// import MySQLTransport from 'winston-mysql/lib/mysql_transport.js';
 
 const options_default = {
     host: process.env.DB_HOST,
@@ -14,6 +14,6 @@ export const logger = winston.createLogger({
     format: winston.format.json(),
     transports: [
         // new winston.transports.Console({}),
-        new MySQLTransport(options_default),
+        // new MySQLTransport(options_default),
     ]
 });
